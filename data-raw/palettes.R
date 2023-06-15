11c809b29b46cbdf23cccb4de4df7c0fa421b1b7
Josh M. London <josh.london@noaa.gov>
2023-06-15 08:03:54 -0700

    update Scientific Colour Maps to v8

--- data-raw/palettes.R
+++ data-raw/palettes.R
@@ -1,5 +1,5 @@
 curl::curl_download(
-  "https://zenodo.org/record/5501399/files/ScientificColourMaps7.zip",
+  "https://zenodo.org/record/8035877/files/ScientificColourMaps8.zip",
   "./data-raw/raw_palettes.zip"
 )
 
@@ -10,7 +10,7 @@ palettes <- list(
 )
 
 palette_names <- list.dirs(
-  "./data-raw/ScientificColourMaps7/", 
+  "./data-raw/ScientificColourMaps8/", 
   full.names = FALSE, 
   recursive = FALSE
 )
@@ -18,17 +18,17 @@ palette_names <- list.dirs(
 palette_names <- grep("^\\w", palette_names, value = TRUE)
 
 for (name in palette_names) {
-  standard_pal <- paste0("./data-raw/ScientificColourMaps7/", name, "/", name, ".txt")
+  standard_pal <- paste0("./data-raw/ScientificColourMaps8/", name, "/", name, ".txt")
   if (file.exists(standard_pal)) {
     palettes[[name]] <- read.table(standard_pal, sep = " ", col.names = c("r", "g", "b"))
   }
-  categorical_pal <- paste0("./data-raw/ScientificColourMaps7/", name, "/CategoricalPalettes/", name, "S_HEX.txt")
+  categorical_pal <- paste0("./data-raw/ScientificColourMaps8/", name, "/CategoricalPalettes/", name, "S_HEX.txt")
   if (file.exists(categorical_pal)) {
     palettes$.categorical[[name]] <- read.table(categorical_pal, skip = 2, comment.char = "", col.names = c("r", "g", "b", "name", "hex"))$hex
   }
 }
 
 unlink("./data-raw/raw_palettes.zip")
-unlink("./data-raw/ScientificColourMaps7/", recursive = TRUE)
+unlink("./data-raw/ScientificColourMaps8/", recursive = TRUE)
 
 usethis::use_data(palettes, overwrite = TRUE, internal = TRUE)
